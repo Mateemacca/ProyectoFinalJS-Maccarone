42682e1b4d1f5f51e8c3cc261e3d801b5993b4a9
Mate Maccarone <mateobauti.maccarone@hotmail.com>
2023-08-06 06:52:33 -0300

    dark mode casi terminado

--- JS/main.js
+++ JS/main.js
@@ -464,7 +464,7 @@ function actualizarProductosEnCarrito() {
     const productoDiv = document.createElement("div");
     productoDiv.classList.add("producto-carrito");
     productoDiv.innerHTML = `
-    <div class="detalle-producto flex items-center">
+    <div class="detalle-producto flex items-center text-black">
   <img class="imagen-producto" src="imgs/${imagenProducto}" alt="${nombreProducto}">
   <div class="detalle-texto">
     <div class="nombre-cantidad">
@@ -575,3 +575,60 @@ function restarProducto(productoId) {
     localStorage.setItem("carrito", carritoJSON);
   }
 }
+
+//
+//
+//
+
+const storageKey = "theme-preference";
+
+const onClick = () => {
+  // flip current value
+  theme.value = theme.value === "light" ? "dark" : "light";
+
+  setPreference();
+};
+
+const getColorPreference = () => {
+  if (localStorage.getItem(storageKey)) return localStorage.getItem(storageKey);
+  else
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+};
+
+const setPreference = () => {
+  localStorage.setItem(storageKey, theme.value);
+  reflectPreference();
+};
+
+const reflectPreference = () => {
+  document.firstElementChild.setAttribute("data-theme", theme.value);
+
+  document
+    .querySelector("#theme-toggle")
+    ?.setAttribute("aria-label", theme.value);
+};
+
+const theme = {
+  value: getColorPreference(),
+};
+
+// set early so no page flashes / CSS is made aware
+reflectPreference();
+
+window.onload = () => {
+  // set on load so screen readers can see latest value on the button
+  reflectPreference();
+
+  // now this script can find and listen for clicks on the control
+  document.querySelector("#theme-toggle").addEventListener("click", onClick);
+};
+
+// sync with system changes
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+    theme.value = isDark ? "dark" : "light";
+    setPreference();
+  });
